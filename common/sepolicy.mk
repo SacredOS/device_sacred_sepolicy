b1a29de38c3d3d0ee87a27e3c4fa040d20812ad8
Ethan Chen <intervigil@gmail.com>
2017-10-18 19:11:22 -0700

    sepolicy: Move sacred policy to platform
    Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,7 +1,10 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from Sacred
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/sacred/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/sacred/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/sacred/sepolicy/common/private
